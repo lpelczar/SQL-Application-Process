d47fdcec76786daead3834c5c696f6e279cafe69
lpelczar <lukepelczar@gmail.com>
2018-01-18 16:18:43 +0100

    Add clearing console method in view

--- src/ApplicationsView.java
+++ src/ApplicationsView.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class ApplicationsView {
 
+    private static void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public String getUserInput() {
         System.out.print("Choose option: ");
         return getStringInput();
