b36d5c150c55f2dd329e571eb894e4dfd9aebe1a
lpelczar <lukepelczar@gmail.com>
2018-01-18 16:23:42 +0100

    Add simple menu method

--- src/ApplicationsView.java
+++ src/ApplicationsView.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class ApplicationsView {
 
+    public void displayMenu() {
+
+        clearConsole();
+        System.out.println("Welcome to application process app!" +
+                "\nWhat do you want to do?\n" +
+                " 1. Get first_name, last_name from mentors table\n" +
+                " 2. Get the nick_name-s of all mentors working at Miskolc\n"
+                " 0. Exit");
+    }
+
     private static void clearConsole() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
