b937183fd3346e79f5d738e9d1e5a8c27a927e14
lpelczar <lukepelczar@gmail.com>
2018-01-18 16:12:04 +0100

    Add start function to controller

--- src/ApplicationsController.java
+++ src/ApplicationsController.java
@@ -1,2 +1,23 @@
 public class ApplicationsController {
+
+    public void start() {
+        boolean isAppRunning = true;
+
+        while (isAppRunning) {
+            rootView.displayMenu();
+            String userInput = rootView.getUserInput();
+            switch (userInput) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "0":
+                    isAppRunning = false;
+                    break;
+                default:
+                    rootView.displayWrongInputMessage();
+            }
+        }
+    }
+
 }
