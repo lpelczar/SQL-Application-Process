6d421fe48747f269cc72408e93583cd79d6da84d
lpelczar <lukepelczar@gmail.com>
2018-01-18 15:41:06 +0100

    Implement mentors contract class

---
+++ src/data/MentorsContract.java
@@ -0,0 +1,15 @@
+package data;
+
+public class MentorsContract {
+
+    public static final class MentorsEntry {
+        public static final String TABLE_NAME = "mentors";
+        public static final String COLUMN_FIRST_NAME = "first_name";
+        public static final String COLUMN_LAST_NAME = "last_name";
+        public static final String COLUMN_NICK_NAME = "nick_name";
+        public static final String COLUMN_PHONE_NUMBER = "phone_number";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_CITY = "city";
+        public static final String COLUMN_FAVOURITE_NUMBER = "favourite_number";
+    }
+}
