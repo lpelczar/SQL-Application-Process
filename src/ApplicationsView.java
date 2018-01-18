d7fd8fbe99c3c8fa2fe50b64887656417c954c5b
lpelczar <lukepelczar@gmail.com>
2018-01-18 16:16:00 +0100

    Add getting input from user in view

--- src/ApplicationsView.java
+++ src/ApplicationsView.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class ApplicationsView {
+
+    public String getUserInput() {
+        System.out.print("Choose option: ");
+        return getStringInput();
+    }
+
+    private String getStringInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
 }
