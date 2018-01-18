6e8209207af8bcd7d5822e4b6978b9f82ef02554
lpelczar <lukepelczar@gmail.com>
2018-01-18 17:58:54 +0100

    Add phone number to applicants contract

--- src/data/ApplicantsContract.java
+++ src/data/ApplicantsContract.java
@@ -6,6 +6,7 @@ public class ApplicantsContract {
         public static final String TABLE_NAME = "applicants";
         public static final String COLUMN_FIRST_NAME = "first_name";
         public static final String COLUMN_LAST_NAME = "last_name";
+        public static final String COLUMN_PHONE_NUMBER = "phone_number";
         public static final String COLUMN_EMAIL = "email";
         public static final String COLUMN_APPLICATION_CODE = "application_code";
     }
