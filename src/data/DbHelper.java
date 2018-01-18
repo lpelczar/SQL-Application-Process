991ef036716d6aa7fa72aea7fdb6719623ed7ddc
lpelczar <lukepelczar@gmail.com>
2018-01-18 17:02:40 +0100

    Add DbHelper class with connecting to the database

---
+++ src/data/DbHelper.java
@@ -0,0 +1,26 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbHelper {
+
+    private static final String DATABASE_NAME = "applications.db";
+
+    public Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        return connection;
+    }
+
+    
+}
